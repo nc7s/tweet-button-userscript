8a2f8e7844b3b348123a0406bdbc9c5de8072a0f
Blair Noctis <code@noctis.im>
2021-03-10 15:23:08 +0800

    feat: Add TweetDeck support; add @downloadURL

--- 鳴叫！.user.js
+++ 鳴叫！.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name			鳴叫！
 // @namespace		https://noctis.im
-// @version	  		0.1
-// @description		把你的 Twitter 頁面上的“发推”按鈕換成“鳴叫！”
+// @version	  		0.2
+// @description		将您 Twitter 網頁版的“推文”按鈕改爲“鳴叫！”。
 // @author			Blair Noctis
 // @match			https://twitter.com/*
 // @match			https://mobile.twitter.com/*
+// @match			https://tweetdeck.twitter.com/*
 // @grant			window.onurlchange
+// @downloadURL		https://raw.githubusercontent.com/bnoctis/tweet-button-userscript/main/%E9%B3%B4%E5%8F%AB%EF%BC%81.user.js
 // ==/UserScript==
 
 (function() {
@@ -19,6 +21,9 @@
 	var sidebarTweetButtonSpanSelector = sidebarTweetButtonSelector + ' span'
 	var inlineTweetButtonSpanSelector = 'div[data-testid="tweetButtonInline"] span'
 	var modalTweetButtonSpanSelector = 'div[data-testid="tweetButton"] span'
+	var tweetDeckCTAButtonSelector = 'button.js-show-drawer.tweet-button'
+	var tweetDeckCTAButtonSpanSelector = 'span[data-test-id="tweet-call-to-action"]'
+	var tweetDeckSendButtonSelector = 'button.js-send-button'
 
 	var sidebarButtonListener
 
@@ -37,6 +42,21 @@
 		console.log('[鳴叫！] replacement run #' + (++totalRunCount).toString())
 		/* 行動版 Twitter 網頁只在發推頁面有一個“推文”按鈕 */
 		if(window.location.hostname === 'mobile.twitter.com' && window.location.pathname !== '/compose/tweet') return
+		if(window.location.hostname === 'tweetdeck.twitter.com') {
+			var tweetDeckCTAButton = $(tweetDeckCTAButtonSelector)
+			if(!tweetDeckCTAButton) return setTimeout(鳴叫, periodicInterval)
+			$(tweetDeckCTAButtonSpanSelector).textContent = text
+			tweetDeckCTAButton.addEventListener('click', () => {
+				var i = setInterval(() => {
+					var tweetDeckSendButton = $(tweetDeckSendButtonSelector)
+					if(tweetDeckSendButton) {
+						tweetDeckSendButton.textContent = text
+						clearInterval(i)
+					}
+				}, periodicInterval)
+			})
+			return
+		}
 		var sidebarButton = $(sidebarTweetButtonSelector)
 		var sidebarSpan = $(sidebarTweetButtonSpanSelector)
 		var inlineSpan = $(inlineTweetButtonSpanSelector)
